d12413d06cd88c11a2a2f277282aa913bc75855b
PABLO_FERNANDES <pablo.machado@yduqs.com.br>
2023-09-11 16:25:49 -0300

    refct: fix and optmized changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
-import { NgModule } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { pt_PT } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { DemoNgZorroAntdModule } from './ng-zorro-antd-module';
+import { HotelCardsComponent } from './components/hotelcards/hotelcards.component';
+import { CardComponent } from './components/card/card.component';
+import { WifiOutline, ShoppingOutline, ReconciliationFill, PushpinOutline, ShoppingCartOutline, ShopTwoTone, ShoppingFill } from '@ant-design/icons-angular/icons';
 
+const icons = [WifiOutline, ShoppingOutline, ReconciliationFill, PushpinOutline, ShoppingCartOutline, ShoppingFill, ShopTwoTone];
+
+registerLocaleData(pt);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    AppComponent,
+    HotelCardsComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    DemoNgZorroAntdModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NzIconModule,
+    [NzIconModule.forChild(icons)]
+
   ],
-  providers: [],
+  exports: [NzPageHeaderModule],
+
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    { provide: NZ_I18N, useValue: pt_PT }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
